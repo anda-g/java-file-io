146f3512bb8b7305490d1e004a76c0844a19ea49
KungSovannda <kungsovannda@gmail.com>
2025-05-22 08:50:23 +0700

    add: an input and output file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        try(FileOutputStream fileOutputStream = new FileOutputStream("person.txt",true)){
+            System.out.print("[+] Write anything to file: ");
+            String data = new Scanner(System.in).nextLine() + "\n";
+            fileOutputStream.write(data.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream("person.txt")){
+            for (byte b : fileInputStream.readAllBytes()){
+                System.out.print((char) b);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
